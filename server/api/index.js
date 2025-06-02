8bf6d0c07a2e7277a33df1a30a8699f56b4a9f77
Quindart <quang.le@aegona.com>
2025-06-03 00:08:43 +0700

    fix: build deploy

--- server/api/index.js
+++ server/api/index.js
@@ -1,3 +1,7 @@
+import { createServer } from "http";
 import app from "../src/app.js";
 
-export default app;
+export default function handler(req, res) {
+    const server = createServer((req_, res_) => app(req_, res_));
+    server.emit("request", req, res);
+}
