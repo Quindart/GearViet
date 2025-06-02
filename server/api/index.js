870e0dcc960aeb27e234fd92f6ae0a1b0c886394
Quindart <quang.le@aegona.com>
2025-06-03 00:21:16 +0700

    fix: build deploy

--- server/api/index.js
+++
@@ -1,7 +0,0 @@
-import { createServer } from "http";
-import app from "../src/app.js";
-
-export default function handler(req, res) {
-    const server = createServer((req_, res_) => app(req_, res_));
-    server.emit("request", req, res);
-}
--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,10 @@
 import express from "express";
 import cors from "cors";
-import fileUpload from "express-fileupload";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import dotenv from "dotenv";
+import fileUpload from "express-fileupload";
 
 import db from "./config/db/index.js";
 import route from "./routes/index.js";
@@ -14,6 +14,9 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+const PORT = process.env.PORT || 5000;
+const BASE_URL = process.env.BASE_URL || "http://localhost";
+
 const app = express();
 
 db.connect();
@@ -21,13 +24,11 @@ db.connect();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(fileUpload({
-    useTempFiles: true,
-    limits: { fileSize: 50 * 1024 * 1024 },
-    abortOnLimit: true
-}));
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(fileUpload({ useTempFiles: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 route(app);
 
-export default app;
+app.listen(PORT, () => {
+    console.log(`Server is running at ${BASE_URL}:${PORT}`);
+});
