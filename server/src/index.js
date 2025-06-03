ae64a68a3fccac35788c6691813362d18167be30
Quindart <quang.le@aegona.com>
2025-06-03 10:11:35 +0700

    fix: build server upload file

--- server/src/index.js
+++ server/src/index.js
@@ -1,35 +1,53 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
 import path from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-import dotenv from "dotenv";
+import cloudinary from "cloudinary";
 import fileUpload from "express-fileupload";
 
 import db from "./config/db/index.js";
 import route from "./routes/index.js";
 
-dotenv.config();
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const PORT = process.env.PORT
+const BASE_URL = process.env.BASE_URL
 
-const PORT = process.env.PORT || 5000;
-const BASE_URL = process.env.BASE_URL || "http://localhost";
-
-const app = express();
+const app = express()
 
 db.connect();
-
-
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: false,
+    })
+);
+
+app.use(
+    fileUpload({
+        useTempFiles: true,
+        limits: {
+            fileSize: 50 * 1024 * 1024,
+        },
+        abortOnLimit: true,
+    })
+);
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET,
+});
 app.use(express.urlencoded({ extended: false }));
-app.use(fileUpload({ useTempFiles: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 route(app);
 
-app.listen(PORT, () => {
-    console.log(`Server is running at ${BASE_URL}:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server listening at ${BASE_URL}:${PORT}`));
